e3eb753fbfa8c717881115bb8a8e0896dcafe479
AjaySbecse <ajays.becse@gmail.com>
2022-04-03 08:37:32 +0530

    remote mysql added

--- auth/config.php
+++ auth/config.php
@@ -1,15 +1,20 @@
 <?php
-    session_start();
-    $hostname = "localhost";
-    $username = "root";
-    $pwd = "";
-    $dbname = "college_social_network";
+session_start();
+// Developement server
+// $hostname = "localhost";
+// $username = "root";
+// $pwd = "";
+// $dbname = "college_social_network";
 
-    $conn = mysqli_connect($hostname,$username,$pwd,$dbname);
-    if(!$conn){
-        echo 'Connection error: '.mysqli_connect_error();
-    }
-    else{
-        echo '<script>console.log("Database connected successfully");</script>';
-    }
-?>
+//production server
+$hostname = "remotemysql.com";
+$username = "5w9D71QdwF";
+$pwd = "R3bFgnsed2";
+$dbname = "5w9D71QdwF";
+
+$conn = mysqli_connect($hostname, $username, $pwd, $dbname);
+if (!$conn) {
+    echo 'Connection error: ' . mysqli_connect_error();
+} else {
+    echo '<script>console.log("Database connected successfully");</script>';
+}
